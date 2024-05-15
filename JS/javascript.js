52f0f77b9056a3774f22e4dcf5dbc2be8d1c4c65
Tecno Espectro <TecnoEspectro15@hotmail.com>
2024-05-15 13:12:36 -0500

    hoy

--- JS/javascript.js
+++ JS/javascript.js
@@ -42,3 +42,60 @@ function bonificacion(){
     console.log("Al empleado se le pago "+valor);
 
 }
+
+var diaDeLaSemana = "viernes";
+switch (diaDeLaSemana) {
+ case "lunes":
+ console.log("Hoy es lunes.");
+ break;
+ case "martes":
+ console.log("Hoy es martes.");
+ break;
+ case "miércoles":
+ console.log("Hoy es miércoles.");
+ break;
+ case "jueves":
+ console.log("Hoy es jueves.");
+ break;
+ case "viernes":
+ console.log("Hoy es viernes.");
+ break;
+ case "sábado":
+ console.log("Hoy es sábado.");
+ break;
+ case "domingo":
+ console.log("Hoy es domingo.");
+ break;
+ default:
+ console.log("No es un día válido.");
+}
+
+for (let i = 1; i <= 5; i++) {
+    console.log(i);
+   }
+
+   let j = prompt("ingrese el numero","");
+   while (j <= 10) {
+    var num = num + j;
+    console.log(num);
+    j++;
+   }
+
+   let k = 1;
+   do {
+    console.log(k);
+    k++;
+   } while (k <= 5);
+   
+   let factor = prompt("Ingrese numero",);
+   var ss = 0;
+   var cc = 1;
+   do{ss= ss*cc}while(cc<=factor);
+i=1;
+   for(i<=50){
+    var parr = 0;
+    var impar =0;
+var resul = i%2
+if(resul == 0){parr++}else{impar++}
+i++}
+console.log("la cantidad de numeros pares fueron " + parr + "y los impares " + impar);
